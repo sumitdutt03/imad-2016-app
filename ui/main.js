377de58b9b7ee17793c563fb63a8419a8c058584
sumitdutt03 <sumitdutt03@gmail.com>
2016-09-21 15:51:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text1');
 element.innerHTML =('Iam your father and my name is luffy');
+var img = document.getELEMENTbyId('img');
+img.onclick = function(){
+img.style.marginleft ='100px';    
+    
+};
