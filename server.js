0dc12b39ec6a92644469ec1df3d877ec96e02bc8
sumitdutt03 <sumitdutt03@gmail.com>
2016-09-21 15:07:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/sumit1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'sumit1.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
