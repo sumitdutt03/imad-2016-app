ba7e80ba85f2ae4a356d535998cca0cfdb08c552
sumitdutt03 <sumitdutt03@gmail.com>
2016-09-22 16:43:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var button = document.getElementById('counter');
-var counter =0;
+
 button.onclick = function(){
     request = new XMLHttpRequest();
     request.onreadystatechange = function()
@@ -9,6 +9,7 @@ button.onclick = function(){
       {
           if(request.status==200)
           {
+              
             counter = counter +1;
             var span =document.getElementById('count');
             span.innerHTML=counter.toString();  
