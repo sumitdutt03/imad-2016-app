8e1bd5029d284a674262072ff7790bb7d89ded04
sumitdutt03 <sumitdutt03@gmail.com>
2016-10-30 11:23:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'sumitdutt03',
+    database :'sumitdutt03',
+    host:'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
 
 var app = express();
 app.use(morgan('combined'));
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pooL = new Pool(config);
+app.get('/test-db',function(req,res) {
+    pool.query('SELECT * FROM test', function(err,result){
+        
+        if(err){
+            res.status(500).send(err.toString());
+        } 
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
+    
+});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
