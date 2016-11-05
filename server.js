66024f4fab9d2e2304c363278db7acd3219b5ef4
sumitdutt03 <sumitdutt03@gmail.com>
2016-11-05 10:57:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/test-db', function (req, res) {
     pool.query('SELECT * FROM test', function(err,result){
         if(err){
             res.status(500).send(err.toString());
-        }esle{
+        }else{
             res.send(strinfify(result));
         }
     });
