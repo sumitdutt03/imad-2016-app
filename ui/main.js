7301ddbcb8bd85d6d89140da64d2ef2c0d656bdc
sumitdutt03 <sumitdutt03@gmail.com>
2016-11-12 23:01:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-///////////////////11-11-2016
+
 var submit = document.getElementById('submit_btn');
     submit.onclick = function () {
         // Create a request object
@@ -21,7 +21,7 @@ var submit = document.getElementById('submit_btn');
         };
 
         
-         var username = document.getElementById('username').value;
+        var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
@@ -49,26 +49,4 @@ var submit = document.getElementById('submit_btn');
 
 
 
-/////////////////
-console.log('Loaded!');
-var button = document.getElementById('counter');
-
-button.onclick = function(){
-    request = new XMLHttpRequest();
-    request.onreadystatechange = function()
-    {
-      if(request.readyState===XMLHttpRequest.DONE)
-      {
-          if(request.status===200)
-          {
-              
-           var counter = request.responseText;
-            var span =document.getElementById('count');
-            span.innerHTML=counter.toString();  
-          }
-      }
-    };
-    request.open('GET','http://sumitdutt03.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
 
